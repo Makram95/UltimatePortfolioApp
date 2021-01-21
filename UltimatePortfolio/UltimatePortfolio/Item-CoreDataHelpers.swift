9a9e65dfe9b250bb678b82c64d46f53b4eb4972e
Makram95 <marc.kramer95@icloud.com>
2021-01-21 13:53:03 +0100

    5: Cleaning up Core Data
    added extensions for Project&Items

---
+++ UltimatePortfolio/UltimatePortfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,34 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Marc on 21.01.21.
+//
+
+import Foundation
+
+extension Item {
+    
+    var itemTitle: String {
+        title ?? ""
+    }
+    var itemDetail: String {
+        detail ?? ""
+    }
+    var itemCreationDate: Date{
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controler = DataController(inMemory: true)
+        let viewContext = controler.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = " This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+}
---
+++ UltimatePortfolio/UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,70 @@
+//
+//  Projects-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Marc on 21.01.21.
+//
+
+import Foundation
+
+
+extension Project {
+    
+    var projectTitle: String {
+        title ?? ""
+    }
+    var projectDetail: String {
+        detail ?? ""
+    }
+    var projectCreationDate: Date{
+        creationDate ?? Date()
+    }
+    var projectColor: String{
+        color ?? "Light Blue"
+    }
+    var projectItems: [Item]{
+        let itemsArray = items?.allObjects as? [Item] ?? []
+        
+    
+        return itemsArray.sorted{ first, second in
+            if first.completed == false {
+                if second.completed {
+                    return true
+                }
+            }else if first.completed {
+                if second.completed == false{
+                    return false
+                }
+            }
+            if first.priority > second.priority{
+                return true
+            }else if first.priority < second.priority{
+                return false
+            }
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    
+    var completionAmmount: Double{
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard originalItems.isEmpty == false else {
+            return 0
+        }
+        let completed = originalItems.filter(\.completed)
+        return Double(completed.count)/Double(originalItems.count)
+    }
+    
+    static var example: Project {
+        let controler = DataController(inMemory: true)
+        let viewContext = controler.container.viewContext
+        
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = " This is an example Project"
+        project.closed = true;
+        project.creationDate = Date()
+        
+        return project
+    }
+
+}

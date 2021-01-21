8e2a70594a032e3aee49459a259936460f4107e4
Makram95 <marc.kramer95@icloud.com>
2021-01-21 14:42:42 +0100

    7. Editing items
    Added possibility to change items

---
+++ UltimatePortfolio/UltimatePortfolio/ItemRowView.swift
@@ -0,0 +1,23 @@
+//
+//  ItemRowView.swift
+//  UltimatePortfolio
+//
+//  Created by Marc on 21.01.21.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}

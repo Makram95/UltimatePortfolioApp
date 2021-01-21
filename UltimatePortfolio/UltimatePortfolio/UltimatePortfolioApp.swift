06b36b742f07f43f36b4b7f28883b0849a32c303
Makram95 <marc.kramer95@icloud.com>
2021-01-21 11:27:53 +0100

    Initial Setup
    Empty Project

---
+++ UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Marc on 21.01.21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

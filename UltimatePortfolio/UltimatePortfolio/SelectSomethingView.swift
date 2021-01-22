67fe9d7b13ec566ee0c8c1f02ecf9f2470e1ac90
Makram95 <marc.kramer95@icloud.com>
2021-01-22 14:57:39 +0100

    14. Finishing ProjectsView

---
+++ UltimatePortfolio/UltimatePortfolio/SelectSomethingView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectSomethingView.swift
+//  UltimatePortfolio
+//
+//  Created by Marc on 22.01.21.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}

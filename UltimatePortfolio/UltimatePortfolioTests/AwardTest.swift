247d2eefbc5dc2f3be4e4d736241c6213a119589
Makram95 <marc.kramer95@icloud.com>
2021-01-27 14:20:03 +0100

    27. Testing the basics
    28. Testing Core Data

---
+++ UltimatePortfolio/UltimatePortfolioTests/AwardTest.swift
@@ -0,0 +1,72 @@
+//
+//  AwardTest.swift
+//  UltimatePortfolioTests
+//
+//  Created by Marc on 27.01.21.
+//
+
+import XCTest
+import CoreData
+@testable import UltimatePortfolio
+
+class AwardTest: BaseTestCase {
+    let awards = Award.allAwards
+
+    func testAwardIDMatchesName(){
+        for award in awards{
+            XCTAssertEqual(award.id, award.name, "Award ID should always match its name.")
+        }
+    }
+
+    func testNewUserHasNoAwards(){
+        for award in awards{
+            XCTAssertFalse(dataController.hasEarned(award: award), "New users should not have earned awards")
+        }
+    }
+
+    func testAddingItems(){
+        let values = [1,10,20,50,100,250,500,1000]
+
+        for (count, value) in values.enumerated(){
+            var items = [Item]()
+
+            for _ in 0..<value{
+                let item = Item(context: managedObjectContext)
+                items.append(item)
+            }
+
+            let matches = awards.filter{ award in
+                award.criterion == "items" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1)
+            for item in items{
+                dataController.delete(item)
+            }
+        }
+    }
+
+    func testCompletingItems(){
+        let values = [1,10,20,50,100,250,500,1000]
+
+        for (count, value) in values.enumerated(){
+            var items = [Item]()
+
+            for _ in 0..<value{
+                let item = Item(context: managedObjectContext)
+                item.completed = true
+                items.append(item)
+            }
+
+            let matches = awards.filter{ award in
+                award.criterion == "complete" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1)
+            for item in items{
+                dataController.delete(item)
+            }
+        }
+    }
+
+}
---
+++ UltimatePortfolio/UltimatePortfolioTests/UltimatePortfolioTests.swift
@@ -0,0 +1,20 @@
+//
+//  UltimatePortfolioTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by Marc on 27.01.21.
+//
+import CoreData
+import XCTest
+@testable import UltimatePortfolio
+
+class BaseTestCase: XCTestCase {
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func setUpWithError() throws {
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
+    }
+
+}
